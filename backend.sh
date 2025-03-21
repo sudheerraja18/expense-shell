b5c214b914c580e360acd4074e2e764921ba1d89
sudheeraja18 <gurramsudheerraja@gmail.com>
2025-03-21 23:02:54 +0530

    Expense Shell

--- backend.sh
+++ backend.sh
@@ -35,56 +35,52 @@ echo "Script started excuting at : $TIMESTAMP" &>>$LOGS_FILE_NAME
 CHECK_ROOT
 
 dnf module disable nodejs -y &>>$LOGS_FILE_NAME
-VALIDATE $? "Disable default NodeJS"
+VALIDATE $? "Disabling existing  default NodeJS"
 
 dnf module enable nodejs:20 -y &>>$LOGS_FILE_NAME
-VALIDATE $? "Enable NodeJS"
+VALIDATE $? "Enabling NodeJS 20"
 
 dnf install nodejs -y &>>$LOGS_FILE_NAME
-VALIDATE $? "Install NodeJS"
+VALIDATE $? "Installing NodeJS"
 
 id expense &>>$LOGS_FILE_NAME
 if [ $? -ne 0 ]
 then
     useradd expense &>>$LOGS_FILE_NAME
-    VALIDATE $? "Add expense user"
+    VALIDATE $? "Adding expense user"
 else
     echo "User expence already added"
 fi
 
 mkdir -p /app 
-VALIDATE $? "Create app directory"
+VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGS_FILE_NAME
-VALIDATE $? "Download the application"
+VALIDATE $? "Downloading backend"
 
 cd /app
 
 rm -rf /app/*
 
 unzip /tmp/backend.zip &>>$LOGS_FILE_NAME
-VALIDATE $? "Unzip the backend application"
+VALIDATE $? "Unzip backend"
 
 npm install &>>$LOGS_FILE_NAME
-VALIDATE $? "Install dependencies"
+VALIDATE $? "Installing dependencies"
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGS_FILE_NAME
-VALIDATE $? "Copy backend service"
-
-systemctl daemon-reload &>>$LOGS_FILE_NAME
-VALIDATE $? "Reload daemon"
-
-systemctl start backend &>>$LOGS_FILE_NAME
-VALIDATE $? "Start backend"
-
-systemctl enable backend &>>$LOGS_FILE_NAME
-VALIDATE $? "Enable backend"
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
 
 dnf install mysql -y &>>$LOGS_FILE_NAME
 VALIDATE $? "Installing MySQL"
 
 mysql -h db.harvargurram.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOGS_FILE_NAME
-VALIDATE $? "Load backend schema"
+VALIDATE $? "Setting up trnsactions schema and tables"
+
+systemctl daemon-reload &>>$LOGS_FILE_NAME
+VALIDATE $? "Reload daemon"
+
+systemctl enable backend &>>$LOGS_FILE_NAME
+VALIDATE $? "Enable backend"
 
 systemctl restart backend &>>$LOGS_FILE_NAME
 VALIDATE $? "Restart backend"
